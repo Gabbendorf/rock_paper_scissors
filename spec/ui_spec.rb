39c19b3a1a5b9ea521f1c1d33d7b4e5faaeca4f9
Gabriella <gabriellamds@yahoo.it>
2017-02-27 13:59:08 +0000

    committed spec folder

--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -6,13 +6,43 @@ RSpec.describe Ui do
   let(:input) {StringIO.new}
   let(:ui) {Ui.new(input, output)}
 
-  def set_up_ui(string)
-    input = StringIO.new(string)
+  # let(:pipe_out) {MyOutputPipe.new}
+  # let(:ui) {Ui.new(input, output)}
+
+  it "says hi" do
     ui = Ui.new(input, output)
+    ui.welcome_player
+    expect(output.string).to include("Welcome to Rock-Paper-Scissors!")
   end
 
+  it "gives the user all possible game options" do
+    input = StringIO.new("\n")
+    ui = Ui.new(input, output)
+    ui.choice
+    expect(output.string).to include
+    ("Make your first choice: rock, paper or scissors?\n")
   end
 
-
-
+#
+#   it "says hi" do
+#     ui = Ui.new("double_pipe_in", pipe_out)
+#     ui.welcome_user
+#     expect(pipe_out.get_contents).to include("Welcome to Rock-Paper-Scissors!")
+#   end
+#
+# end
+# #
+# #
+# class MyOutputPipe
+#   def initialize
+#     @contents = ""
+#   end
+#
+#   def puts message
+#     @contents += message
+#   end
+#
+#   def get_contents
+#     @contents
+#   end
 end
